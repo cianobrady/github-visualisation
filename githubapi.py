6532f1edcbb9529ccd4813390fec4401c5ab66e7
Cian O'Brady <cianobrady7@gmail.com>
2018-11-30 12:45:37 +0000

    Completed task of interrogating api and creating json file (json upload is a sample upload

--- githubapi.py
+++ githubapi.py
@@ -1,17 +1,41 @@
 from github import Github
 import getpass
-
+import json
 
 user = input("Enter your username: ")
 
 password = getpass.getpass(prompt = "Enter your password: ", stream=None)
 g = Github(user, password)
 
-for repo in g.get_user().get_repos():
+username = input("Enter github username: ")
+for repo in g.get_user(username).get_repos():
     print(repo.name)
-    contributors = repo.get_contributors()
-    count = 0
-    for contributor in contributors:
-        count = count + 1
-    print(count)
-        #print(contributor.login)
+
+repository = input("Enter repo: ")
+data = []
+for repo in g.get_user(username).get_repos():
+    if(repository==repo.name):
+        z = repo.get_stats_contributors()
+        total_commits = 0
+        total_additions = 0
+        total_deletions = 0
+        for x in z:
+            for week in x.weeks:
+                total_commits += week.c
+                total_additions += week.a
+                total_deletions += week.d
+        net_lines = total_additions-total_deletions
+        for x in z:
+            login = x.author.login
+            total = 0
+            additions = 0
+            deletions = 0
+            for week in x.weeks:
+                total += week.c
+                additions += week.a
+                deletions += week.d
+            data.append({'user': login,'percentages': [{'value': total*100/total_commits,'type': 'commits'},{'value': (additions-deletions)*100/net_lines,'type': 'loc'}]})
+
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
